f994a059478457f1c2075cdabca681e2b9b242e2
KaiGartenschlaeger <kai.gartenschlaeger@awin.com>
2020-01-16 09:39:35 +0100

    Added test project

---
+++ PureFreak.ObjectMapper.Tests/ObjectMapperTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PureFreak.ObjectMapper.Tests
+{
+    [TestClass]
+    public class ObjectMapperTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
