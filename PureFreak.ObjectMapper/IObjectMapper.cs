3b4708bce495b9e99c906fd8a0449c45a39c4fc2
KaiGartenschlaeger <kai.gartenschlaeger@awin.com>
2020-01-16 09:24:40 +0100

    Initial version

---
+++ PureFreak.ObjectMapper/IObjectMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace PureFreak.ObjectMapper
+{
+    public interface IObjectMapper
+    {
+        T Map<T>(object source);
+
+        void MapProperties<TSource, TTarget>(TSource source, TTarget target);
+    }
+}
---
+++ PureFreak.ObjectMapper/ObjectMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PureFreak.ObjectMapper
+{
+    public class ObjectMapper : IObjectMapper
+    {
+        private IList<PropertyMap> GetMatchingProperties(Type sourceType, Type targetType)
+        {
+            var sourceProperties = sourceType.GetProperties();
+            var targetProperties = targetType.GetProperties();
+
+            var properties = (from s in sourceProperties
+                              from t in targetProperties
+                              where s.Name == t.Name &&
+                                    s.CanRead &&
+                                    t.CanWrite &&
+                                    s.PropertyType == t.PropertyType
+                              select new PropertyMap
+                              {
+                                  SourceProperty = s,
+                                  TargetProperty = t
+                              }).ToList();
+
+            return properties;
+        }
+
+        public T Map<T>(object source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var sourceType = source.GetType();
+            var targetType = typeof(T);
+
+            var matchingProperties = GetMatchingProperties(sourceType, targetType);
+
+            var result = Activator.CreateInstance<T>();
+
+            foreach (var match in matchingProperties)
+            {
+                var value = match.SourceProperty.GetValue(source);
+                match.TargetProperty.SetValue(result, value);
+            }
+
+            return result;
+        }
+
+        public void MapProperties<TSource, TTarget>(TSource source, TTarget target)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
