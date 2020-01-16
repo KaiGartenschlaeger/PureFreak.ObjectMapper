4d4e3f0dd667d61391963f7808a97bd680d0476c
KaiGartenschlaeger <kai.gartenschlaeger@awin.com>
2020-01-16 12:28:55 +0100

    Added test for MapProperties

--- PureFreak.ObjectMapper.Tests/ObjectMapperTests.cs
+++ PureFreak.ObjectMapper.Tests/ObjectMapperTests.cs
@@ -15,24 +15,44 @@ namespace PureFreak.ObjectMapper.Tests
 
         class TargetEntity
         {
+            public int Id { get; set; }
             public string Username { get; set; }
             public string Password { get; set; }
         }
 
-        [TestMethod]
-        public void ShouldCopyObject()
+        private SourceEntity CreateSourceEntity()
         {
-            var source = new SourceEntity
+            return new SourceEntity
             {
                 Id = 10,
                 Firstname = "Max",
                 Lastname = "Mustermann",
                 Username = "MaxMustermann"
             };
+        }
+
+        [TestMethod]
+        public void ShouldCopyObject()
+        {
+            var source = CreateSourceEntity();
 
             IObjectConverter converter = new ObjectConverter();
             var target = converter.Convert<TargetEntity>(source);
 
+            Assert.AreEqual(source.Id, target.Id);
+            Assert.AreEqual(source.Username, target.Username);
+        }
+
+        [TestMethod]
+        public void ShouldCopyProperties()
+        {
+            var source = CreateSourceEntity();
+            var target = new TargetEntity();
+
+            IObjectConverter converter = new ObjectConverter();
+            converter.MapProperties(source, target);
+
+            Assert.AreEqual(source.Id, target.Id);
             Assert.AreEqual(source.Username, target.Username);
         }
     }
