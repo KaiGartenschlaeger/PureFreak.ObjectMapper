ac93c0b2c9043370525ab5a74556d805c04df27e
KaiGartenschlaeger <kai.gartenschlaeger@awin.com>
2020-01-16 13:17:17 +0100

    Added service collection extensions

---
+++ PureFreak.ObjectMapper/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using PureFreak.ObjectMapper;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddObjectMapper(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.TryAdd(ServiceDescriptor.Singleton<IObjectConverter, ObjectConverter>());
+
+            return services;
+        }
+    }
+}
